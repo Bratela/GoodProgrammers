a77c9b280101d64a1d7f925c5f78cd6963382aa4
Bratela <den.mitrofanov@gmail.com>
2015-10-20 22:49:55 +0300

    Initial commit 1 Part Lesson 15

---
+++ lesson15/send_mail.rb
@@ -0,0 +1,35 @@
+require "pony"
+require "io/console"
+
+my_mail ="sitecreator1812@ya.ru"
+
+puts "Введите пароль от вашей почты #{my_mail} для отправки письма:"
+password =STDIN.noecho(&:gets).chomp
+puts "Кому отправить письмо ?"
+send_to =STDIN.gets.chomp
+
+puts "Что написать в письме?"
+body =STDIN.gets.chomp
+
+Pony.mail(
+    {
+        :subject => "Привет из Ruby",
+        :body => body,
+        :to => send_to,
+        :from => my_mail,
+
+        :via => :smtp,
+        :via_options => {
+            :address        => 'smtp.yandex.ru',
+            :port           => '465',
+            :tls            => true ,
+            :user_name      => my_mail,
+            :password       => password,
+            :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
+            #:domain         => "localhost.localdomain" # the HELO domain provided by the client to the server
+        }
+
+
+          }
+)
+puts "Письмо успешно отправлено!"
